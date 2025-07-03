7cae14990ff738f6c5813913ff42753deac3765e
lashihollis <105299696+lashihollis@users.noreply.github.com>
2025-07-03 13:26:12 -0400

    updating things

--- practice_projects/caresync-app/server/src/index.ts
+++ practice_projects/caresync-app/server/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import appointmentsRouter from './routes/appointments'; 
 
 const app = express();
 app.use(cors());
@@ -8,6 +9,8 @@ app.use(express.json());
 app.get('/', (_req, res) => {
   res.send('Welcome to CareSync API');
 });
+
+app.use('/appointments', appointmentsRouter);
 
 app.listen(3001, () => {
   console.log('Server is running on http://localhost:3001');
