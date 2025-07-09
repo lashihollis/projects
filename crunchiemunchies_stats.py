0b1ac9b84e2f77e904ed647045ba0b13ad37b72a
lashihollis <105299696+lashihollis@users.noreply.github.com>
2025-07-09 14:38:16 -0400

    Update crunchiemunchies_stats.py
    edited how to find nth percentile.

--- crunchiemunchies_stats.py
+++ crunchiemunchies_stats.py
@@ -13,16 +13,11 @@ print(calorie_stats_sorted)
 median_calories = np.median(calorie_stats_sorted)
 print(median_calories)
 
-tenth_percentile = np.percentile(calorie_stats_sorted, 10)
-print(tenth_percentile)
-twentieth_percentile = np.percentile(calorie_stats_sorted, 20)
-print(twentieth_percentile)
-thirtieth_percentile = np.percentile(calorie_stats_sorted, 30)
-print(thirtieth_percentile)
-fortieth_percentile = np.percentile(calorie_stats_sorted, 40)
-print(fortieth_percentile)
-
-nth_percentile = tenth_percentile
+for p in range(1, 101):
+    if np.percentile(calorie_stats_sorted, p) > 60:
+        nth_percentile = np.percentile(calorie_stats_sorted, p)
+        break
+print(nth_percentile)
 
 #percentage of values in data set that have more than 60 calories
 more_calories = np.mean(calorie_stats > 60)
@@ -32,4 +27,7 @@ print(more_calories)
 calorie_std = np.std(calorie_stats_sorted)
 print(calorie_std)
 
-#Based on the analysis, the average calorie count of the cereals in the dataset is approximately 107 calories, with a median of 110 calories, suggesting a fairly symmetrical distribution. Most cereals fall within the 100–120 calorie range, and about 96% of them have more than 60 calories per serving. The standard deviation is relatively low, indicating that calorie counts don’t vary much across products. For Yummy Corp, this is an advantage when marketing CrunchieMunchies. If CrunchieMunchies falls at or slightly below the average calorie count (while still being tasty), Yummy Corp can promote it as a delicious, better-for-you option that aligns with what customers are already choosing — potentially even highlighting it as a smart pick for calorie-conscious consumers.
+#Based on the analysis, the average calorie count of the cereals in the dataset is approximately 107 calories, with a median of 110 calories, suggesting a fairly symmetrical distribution. Most cereals fall within the 100–120 calorie range, 
+#and about 96% of them have more than 60 calories per serving. The standard deviation is relatively low, indicating that calorie counts don’t vary much across products. For Yummy Corp, this is an advantage when marketing CrunchieMunchies. 
+#If CrunchieMunchies falls at or slightly below the average calorie count (while still being tasty), 
+#Yummy Corp can promote it as a delicious, better-for-you option that aligns with what customers are already choosing — potentially even highlighting it as a smart pick for calorie-conscious consumers.
